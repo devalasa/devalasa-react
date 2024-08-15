f70387a374cfa7fec0f65a9cb5ea1ca7395b3c07
devalasa <ogb22001@byui.edu>
2024-08-15 08:47:51 -0700

    continaution

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,41 @@
+import { useState } from "react";
+import BlogList from "./blog-list";
 const Home = () => {
+  // // let name = "Mario";
+  // const [name, setName] = useState("Mario");
+  // const handleClick = () => {
+  //   setName("Luigi");
+  // };
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "Mario", id: 1 },
+    { title: "Welcome party", body: "lorem ipsum...", author: "Yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "Mario",
+      id: 3,
+    },
+  ]);
+  // const handleClickAgain = (name, e) => {
+  //   console.log("hello" + name, e.target);
+  // };
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      <BlogList blogs={blogs} title="All Blogs!" />
+      {/* Filter blogs */}
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "Mario")}
+        title="Mario's Blogs!"
+      />
+      {/* <p>{name}</p>
+      <button onClick={handleClick}>Click me</button>
+      {/* <button
+        onClick={(e) => {
+          handleClickAgain("Mario", e);
+        }}
+      >
+        Click me again
+      </button> */}
     </div>
   );
 };
